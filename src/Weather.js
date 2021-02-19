b73a47a3efe0998f9254aa7fdcbb5a1643f17be9
rowangeerdink <76164581+rowangeerdink@users.noreply.github.com>
2021-02-19 15:52:37 +0100

    tried to make search engine work but something is wrong

--- src/Weather.js
+++ src/Weather.js
@@ -6,8 +6,9 @@ import FormattedDate from "./FormattedDate.js";
 export default function Weather(props) {
   const [ready, setReady] = useState(false);
   const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       temperature: response.data.main.temp,
       date: new Date(response.data.dt * 1000),
@@ -19,13 +20,29 @@ export default function Weather(props) {
     setReady(true);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "12087b5c6e656cb621cae20a854dfb64";
+    let unit = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (ready) {
     return (
       <div className="row top-row">
         <div className="col-md-6 weather-today ">
           <h1 className="city"> {props.defaultCity} </h1>
           <p className="DateTimeToday">
-            <FormattedDate date={weatherData.date} />{" "}
+            <FormattedDate date={weatherData.date} />
           </p>
           <p className="weatherIcon">❄️</p>
           <p>
@@ -38,12 +55,13 @@ export default function Weather(props) {
 
         <div className="col-md-6 search-description ">
           <div className="row">
-            <form>
+            <form on submit={handleSubmit}>
               <input
                 type="search"
                 placeholder="Search City"
                 className="form-control search-form"
                 autoComplete="off"
+                onChange={handleCityChange}
               />
 
               <div className="row">
@@ -87,11 +105,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "12087b5c6e656cb621cae20a854dfb64";
-    let unit = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${unit}`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
   }
 }
